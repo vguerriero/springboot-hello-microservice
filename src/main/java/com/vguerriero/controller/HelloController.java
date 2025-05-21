fac692ca62d59b2bb51cd1f9896d8d6833e502fd
Viggo Guerriero <viggo@viggomichael.com>
2025-05-21 13:30:53 -0400

    Add HelloController

---
+++ src/main/java/com/vguerriero/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.vguerriero.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Collections.singletonMap("message", "Hello, World!");
+    }
+}
