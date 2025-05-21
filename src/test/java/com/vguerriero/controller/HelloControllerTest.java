b2b045d393f0b60a3448b2fa9728b500cf67a65a
Viggo Guerriero <viggo@viggomichael.com>
2025-05-21 13:34:01 -0400

    Add MockMvc test for HelloController

---
+++ src/test/java/com/vguerriero/controller/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.vguerriero.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloEndpointReturnsHelloWorld() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"message\":\"Hello, World!\"}"));
+    }
+}
